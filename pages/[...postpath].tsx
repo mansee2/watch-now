46f489e20c9135afa4e10bc20b080c9d738bbaa4
mansee2 <mansee483@gmail.com>
2025-04-29 23:43:35 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,16 +1,8 @@
-import { GetServerSideProps } from 'next';
-
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // Force redirect ALL traffic to majhiladkibahin.in
   return {
     redirect: {
-      permanent: false, // Set to true if this is a permanent redirect (HTTP 301)
-      destination: 'http://majhiladkibahin.in/',
+      permanent: false, // 301 रीडायरेक्ट के लिए `true` करें
+      destination: 'https://majhiladkibahin.in/', // ✅ पूरा URL दें
     },
   };
 };
-
-// Empty component (not needed since we're redirecting)
-export default function Redirect() {
-  return null;
-}
